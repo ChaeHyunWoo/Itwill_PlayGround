5e8e24382fc60cd2af2e831702ad5b76c608c608
채현우 <62530720+chaehyuenwoo@users.noreply.github.com>
2022-01-25 18:14:25 +0900

    SQL

--- SQL/scott.sql
+++ SQL/scott.sql
@@ -7,6 +7,7 @@ SELECT * FROM DEPT; --회사 정보
 
 SELECT * FROM SALGRADE;
 
+--1월24일 문제--------------------------------------------------------------------------------------------------------------------
 
 --2. EMP 테이블에서 급여가 3000이상인 사원의 정보를 사원번호,이름,담당업무,급여를 출력하는 SELECT 문장을 작성하시오.
 SELECT EMPNO, ENAME, JOB, SAL FROM EMP
@@ -58,3 +59,64 @@ WHERE JOB IN('CLERK','ANALYST') AND SAL NOT IN(1000, 3000, 5000);
 --11. EMP 테이블에서 이름에 L이 두 자가 있고  부서가 30이거나 또는 관리자가 7782인 사원의 모든 정보를 출력하는 SELECT 문을 작성하여라.
 SELECT * FROM EMP
 WHERE ENAME LIKE '%L%L%' AND DEPTNO = 30 OR MGR = 7782;
+
+
+---------------------1월 25일 문제----------------------------------------------------------------------------------------------
+SELECT * FROM EMP;
+
+--1. 현재 날짜를 출력하고 열 레이블은 Current Date로 출력하는 SELECT 문장을 기술하시오.
+SELECT SYSDATE AS "Current Date" FROM DUAL;
+ 
+
+--2. EMP 테이블에서 현재 급여에 15%가 증가된 급여를 사원번호,이름,업무,급여,증가된 급여(New Salary),증가액(Increase)를 출력하는 SELECT 문장을 기술하시오.
+SELECT EMPNO, ENAME, JOB, SAL*1.15 AS "New Salary", SAL*0.15 AS "Increase" FROM EMP;
+
+ 
+
+--3. EMP 테이블에서 이름,입사일,입사일로부터 6개월 후 돌아오는 월요일 구하여 출력하는 SELECT 문장을 기술하시오.
+
+SELECT ENAME, TO_CHAR(HIREDATE, 'YYYY-MM-DD')입사일, TO_CHAR(NEXT_DAY(ADD_MONTHS(HIREDATE,6),'월요일'), 'YYYY-MM-DD')
+"6개월 후 월요일" FROM EMP;
+ 
+
+--4. EMP 테이블에서 이름,입사일, 입사일로부터 현재까지의 월수,급여, 입사일부터 현재까지의 급여의 총계를 출력하는 SELECT 문장을 기술하시오.
+
+SELECT ENAME, HIREDATE, ROUND(MONTHS_BETWEEN(SYSDATE, HIREDATE)) "현재까지의 월 수", SAL, 
+ROUND(MONTHS_BETWEEN(SYSDATE, HIREDATE))*SAL"현재까지의 급여" FROM EMP;
+
+
+
+--5. EMP 테이블에서 다음의 결과가 출력되도록 작성하시오.
+
+--CONCAT 이랑 ||
+/*
+Dream Salary
+
+------------------------------------------------------------
+
+KING earns $5,000.00 monthly but wants $15,000.00
+
+BLAKE earns $2,850.00 monthly but wants $8,550.00
+
+CLARK earns $2,450.00 monthly but wants $7,350.00
+
+. . . . . . . . . .
+
+14 rows selected
+ */
+ 
+
+--6. EMP 테이블에서 모든 사원의 이름과 급여(15자리로 출력 좌측의 빈곳은 “*”로 대치)를 출력하는 SELECT 문장을 기술하시오.
+SELECT ENAME, LPAD(SAL, 15, '*') FROM EMP;
+ 
+
+--7. EMP 테이블에서 모든 사원의 정보를 이름,업무,입사일,입사한 요일을 출력하는 SELECT 문장을 기술하시오.
+SELECT ENAME, JOB, HIREDATE, TO_CHAR(HIREDATE, 'DAY') FROM EMP;
+ 
+
+--8. EMP 테이블에서 이름의 길이가 6자 이상인 사원의 정보를 이름,이름의 글자수,업무를 출력하는 SELECT 문장을 기술하시오.
+SELECT ENAME, LENGTH(ENAME), JOB FROM EMP
+WHERE LENGTH(ENAME) >= 6;
+ 
+--9. EMP 테이블에서 모든 사원의 정보를 이름,업무,급여,보너스,급여+보너스를 출력하는 SELECT 문장을 기술하시오.
+SELECT ENAME, JOB, SAL, COMM, NVL(COMM, 0) + SAL FROM EMP;
